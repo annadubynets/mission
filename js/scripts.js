9affad252aa0babcd027f22acc8aa85218fe4011
Ivan Dubynets <ivan.dubynets@gmail.com>
2021-05-31 17:53:16 +0300

    verification modal logic

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,11 @@ $(document).ready(function() {
     });
 });
 
-$('select').selectpicker();
+$(function() {
+    if ($('select').length > 0) {
+        $('select').selectpicker();
+    }
+})
 
 //--------------------------//
 //      Clipboard utils     //
@@ -36,9 +40,30 @@ ClipboardUtils.copyToClipboard = function(text) {
 //--------------------------//
 
 $(function() {
-    $('input[name="daterange"]').daterangepicker({
-        opens: 'left'
-    }, function(start, end, label) {
-        console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
-    });
+    if ($('input[name="daterange"]').length > 0) {
+        $('input[name="daterange"]').daterangepicker({
+            opens: 'left'
+        }, function(start, end, label) {
+            console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+        });
+    }
 });
+
+
+
+$(function() {
+    $('.verification-item').on('click', '.btn-select-file', function() {
+        $(this).closest('.verification-item').find('.file-upload-input').click();
+    });
+
+    $('.file-upload-input').on('change', function(e) {
+        var fileName = e.target.files[0].name;
+
+        var fileNameElem = $(this).closest('.verification-item').find('.file-name');
+        var uploadStatusElem = $(this).closest('.verification-item').find('.upload-status');
+        
+        fileNameElem.text(fileName);
+        fileNameElem.removeClass('d-none');
+        uploadStatusElem.text('Uploaded')
+    })
+})
